49f27e124817dc9fd65fd2baa79aafa09648d2ca
Patrick Ging <patrickging04@gmail.com>
2023-09-11 14:38:22 -0400

    Adding new method

--- parse/parse.go
+++ parse/parse.go
@@ -11,12 +11,43 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
 	"sync"
 )
+
+/*
+readConfigFile
+
+Reads the config.json file and will return it as a
+config struct.
+
+Will not return an error object. Will abort the program if the
+file cannot be processed correctly.
+*/
+func readConfigFile() Config {
+	file, err := os.Open("./config.json")
+	if err != nil {
+		log.Fatal("error: Could not load config.json file")
+	}
+	defer file.Close()
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal("error: Had trouble reading config.json as bytes")
+	}
+
+	var config Config
+	err = json.Unmarshal(bytes, &config)
+	if err != nil {
+		log.Fatal("error: Could not unmarshall config.json")
+	}
+
+	return config
+}
 
 /*
 requestGreenHouseJobs
